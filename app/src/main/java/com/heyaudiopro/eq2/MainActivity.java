89248072540cf79852bccdb5c8d74285ffa278c1
eugene gallego <eugall@gmail.com>
2026-04-17 00:20:06 +0800

    Update MainActivity.java

--- app/src/main/java/com/heyaudiopro/eq2/MainActivity.java
+++ app/src/main/java/com/heyaudiopro/eq2/MainActivity.java
@@ -1,1 +1,22 @@
+package com.heyaudiopro.eq2;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
