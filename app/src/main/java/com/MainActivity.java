71e0d53f49fb29fe511cda779a1211a3c10a8621
eugene gallego <eugall@gmail.com>
2026-04-16 20:22:30 +0800

    Update MainActivity.java

--- app/src/main/java/com/MainActivity.java
+++ app/src/main/java/com/MainActivity.java
@@ -1,1 +1,19 @@
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
 
+    <application
+        android:allowBackup="true"
+        android:label="EQ Player"
+        android:theme="@android:style/Theme.DeviceDefault.NoActionBar">
+
+        <activity android:name=".MainActivity"
+            android:exported="true">
+
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+
+        </activity>
+    </application>
+
+</manifest>
